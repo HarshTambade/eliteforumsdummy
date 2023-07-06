a4c85b8dabcc61dc9716b1a0688ee15ab438c934
Harsh Tambade <85555282+HarshTambade@users.noreply.github.com>
2023-07-06 01:50:43 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Elite Forums",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "eliteforumsindia@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
