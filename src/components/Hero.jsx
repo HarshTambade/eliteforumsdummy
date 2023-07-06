ec0576d54239372a08c3db7ab82ec2841c17e49c
Harsh Tambade <85555282+HarshTambade@users.noreply.github.com>
2023-07-06 01:49:07 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, We are <span className='text-[#915EFF]'>Elite Forums</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            We train students in the field of IT <br className='sm:block hidden' />
+            & also provide IT solutions and consultancy
           </p>
         </div>
       </div>
